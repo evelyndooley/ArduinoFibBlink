c03c768595a79cba6d3b882e678db150e1553ad6
Nate Dooley <nxd7989@rit.edu>
2017-09-03 20:44:22 -0400

    Initial commit

---
+++ ArduinoFibBlink.ino
@@ -0,0 +1,44 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://www.arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+int fib(int n)
+{
+   if (n <= 1)
+      return n;
+   return fib(n-1) + fib(n-2);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  for(int n = 25; n > 0; n--){
+    int fibret = fib(n);
+    Serial.println(fibret);
+    for(int i = 25; i > 0; i--){
+        digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(fibret);              // wait for a second
+        digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+        delay(fibret);              // wait for a second
+    }
+    delay(1000);
+  }
+
+}
